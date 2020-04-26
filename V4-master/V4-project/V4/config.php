70535ae1ed96ddc09cc4dc17934952ee44888487
Jenna Lovett <jennablair12@gmail.com>
2020-04-26 15:33:25 -0400

    removed folders

--- V4-master/V4-project/V4/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'myv4db.coondoclxh8y.us-east-1.rds.amazonaws.com:3306');
-   define('DB_USERNAME', 'v4master');
-   define('DB_PASSWORD', 'v4password');
-   define('DB_DATABASE', 'v4');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
