230145c6d947b8aec22496c2c3d6eda4e1b57898
Jenna Lovett <jennablair12@gmail.com>
2020-04-26 15:31:28 -0400

    Delete config.php

--- V4-master/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'v4.c4pbktdjfsor.us-east-1.rds.amazonaws.com:3306');
-   define('DB_USERNAME', 'V4admin');
-   define('DB_PASSWORD', 'password');
-   define('DB_DATABASE', 'V4');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
